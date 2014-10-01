fc6db687541cfdcf65cb6756d8c32b8ff1735325
Raghu Varma <raghuvarma@masymbol.com>
2014-10-01 10:53:32 +0530

    add data to the fixture file to test our Product model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+	fixtures :products
   # test "the truth" do
   #   assert true
   # end
@@ -40,7 +41,7 @@ class ProductTest < ActiveSupport::TestCase
 		image_url:
 		image_url)
 	end
-	
+
 	test "image url" do
 		ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
 		http://a.b.c/x/y/z/fred.gif }
@@ -52,5 +53,16 @@ class ProductTest < ActiveSupport::TestCase
 			assert new_product(name).invalid?, "#{name} shouldn't be valid"
 		end
 	end
+
+	test "product is not valid without a unique title" do
+		product = Product.new(title:
+		products(:ruby).title,
+		description: "yyy",
+		price:1,
+		image_url:"fred.gif")
+		assert product.invalid?
+		assert_equal ["has already been taken"], product.errors[:title]
+	end
+
 
 end
