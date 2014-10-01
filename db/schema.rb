bf367056be50ff645221695ff270fa782bf9d342
Raghu Varma <raghuvarma@masymbol.com>
2014-10-01 23:27:26 +0530

    add scaffold cart, current_cart.rb in concerns for makes the current session look like a hash to the controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140930181811) do
+ActiveRecord::Schema.define(version: 20141001175159) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
