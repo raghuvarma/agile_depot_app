a6ee7c1a75e962dc52436ae1b7287c2380749258
Raghu Varma <raghuvarma@masymbol.com>
2014-10-05 16:39:41 +0530

    Hiding an Empty Cart with css style display:none and using helper methods

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -61,7 +61,8 @@ class CartsController < ApplicationController
 
     respond_to do |format|
       #format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
-      format.html { redirect_to store_url, notice: 'Your cart is currently empty' }
+      #format.html { redirect_to store_url, notice: 'Your cart is currently empty' }
+      format.html { redirect_to store_url }
       format.json { head :no_content }
     end
   end
