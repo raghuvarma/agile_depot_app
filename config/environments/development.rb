aa4d69c24ecc31d5e1216a4c59b58dc122e278cc
Raghu Varma <raghuvarma@masymbol.com>
2014-10-05 19:48:28 +0530

    add smtp setting for gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -36,4 +38,15 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "www.google.com",
+    authentication: "plain",
+    user_name: "rrv461@gmail.com",
+    password: "raghu461",
+    enable_starttls_auto: true
+  }
+
 end
